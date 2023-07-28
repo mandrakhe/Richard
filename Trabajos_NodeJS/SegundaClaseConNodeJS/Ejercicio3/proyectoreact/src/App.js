cb8f3736f992d56cef81f8a86b1e111458ae2d05
luis fernando <luisfernnandomb@gmail.com>
2023-07-27 21:46:01 -0500

    tercer trabajo terminado

---
+++ Trabajos_NodeJS/SegundaClaseConNodeJS/Ejercicio3/proyectoreact/src/App.js
@@ -0,0 +1,44 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+      <nav className="navbar">
+        <img  src={logo} className="App-logo" alt="logo" />
+      <ul className="navbar-menu">
+        <li className="navbar-item">Inicio</li>
+        <li className="navbar-item">Ayuda</li>
+        <li className="navbar-item">Contacto</li>
+      </ul>
+    </nav>
+    <div className="content">
+        <h1>Estrategias para ganar en Warzone</h1>
+        <p>
+         Pai, en guarson hay como 7 tipos de jugadores: Camperos, Rushers, Tácticos, Escopeteros, Mancos, Medio buenos y Viciados peeeeeeeeeeeeeeeeeeeeeeero pa ganarles vea, le dejo estos laifjaks bien insanos 🥵😈
+        </p>
+        <ul>
+          <li>1. Aterriza en lugares estratégicos: Escoge puntos de aterrizaje con buena visibilidad, suministros cercanos y menos competencia inicial.</li>
+          <li>2. Comunicación en equipo: Mantén una comunicación efectiva con tu equipo para coordinar movimientos y estrategias.</li>
+          <li>3. Recoge dinero y contratos: El dinero te permitirá comprar suministros y revivir compañeros de equipo. Los contratos te darán recompensas valiosas.</li>
+          <li>4. Administra tu armamento: Escoge un conjunto de armas equilibrado y adapta tu loadout según la situación.</li>
+          <li>5. Juega estratégicamente el Círculo: Sé consciente de la zona de seguridad y planifica tus movimientos con anticipación.</li>
+          <li>6. Full rush, el que tenga miedo a morir que no nazca</li>
+          <li>7. Use la combinación más rata: Escudo y la KV Broadside con Cañón: GUNNER D20
+PESO: ↑ +0.50 lb / DURACIÓN: → +0.40 en
+Láser: HIPSHOT L20
+PESO: ↑ +0.48 oz / DISTANCIA A CERO: → +51.00 pies
+Culata: VLK SIN CULATA
+Cerrojo: DASHBOLT 60
+Cargador: TAMBOR CON 25 CARTUCHOS </li>
+          <li>7. Hacks</li>
+        </ul>
+        <img id ="guarson "src="guarson.jpg" alt="Guarson" />
+      </div>
+    </div>
+  );
+}
+
+export default App;
