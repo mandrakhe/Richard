4747357ddbcd82bca1b443c905446c42073355d7
luis fernando <luisfernnandomb@gmail.com>
2023-08-17 07:46:16 -0500

    Cuarta clase con richard MEAN

---
+++ Trabajos_NodeJS/CuartaClase/MEAN/servidor/controllers/productoController.js
@@ -0,0 +1,24 @@
+const Producto = require ("../models/Producto");
+
+exports.crearProducto = async (req,res)=>{
+    try {
+        let producto;
+        producto = new Producto(req.body);
+        await producto.save();
+        res.send(producto);
+    }catch(error){
+        console.log(error);
+        res.status(500).send('error')
+    }
+}
+
+exports.obtenerProductos = async (req,res)=>{
+    try{
+       const productos = await Producto.find();
+       res.json(productos);  
+    }
+    catch(error){
+        console.log(error);
+        res.status(500).send('error')
+}
+}
