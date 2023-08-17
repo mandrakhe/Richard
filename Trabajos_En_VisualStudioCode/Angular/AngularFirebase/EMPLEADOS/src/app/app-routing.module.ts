6c52725dde60db6d5e3783518de507a9d223b5ec
luis fernando <luisfernnandomb@gmail.com>
2023-08-17 18:09:49 -0500

    update Firebase

---
+++ Trabajos_En_VisualStudioCode/Angular/AngularFirebase/EMPLEADOS/src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ListEmployeeComponent } from './components/list-employee/list-employee.component';
+import { CreateEmployeeComponent } from './components/create-employee/create-employee.component';
+
+const routes: Routes = [
+{path: '', component:ListEmployeeComponent,pathMatch:'full'},
+{path:'list-employee', component:ListEmployeeComponent},
+{path:'list-employee', component:CreateEmployeeComponent},
+{path:'**',component:ListEmployeeComponent,pathMatch:'full'},
+
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ Trabajos_En_VisualStudioCode/Angular/AngularFirebase/EMPLEADOS/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ListEmployeeComponent } from './components/list-employee/list-employee.component';
+import { CreateEmployeeComponent } from './components/create-employee/create-employee.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ListEmployeeComponent,
+    CreateEmployeeComponent,
+    NavbarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
