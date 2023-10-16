85dafd9191a33b660ace2e5b4996371a7c0e76f6
mandrakhe <luisfernnandomb@gmail.com>
2023-10-15 22:22:40 -0500

    5to video

--- Trabajos_En_VisualStudioCode/Practical_clase/LOGIN-MRN/src/routes/task.routes.js
+++ Trabajos_En_VisualStudioCode/Practical_clase/LOGIN-MRN/src/routes/task.routes.js
@@ -1,12 +1,14 @@
 import {Router} from 'express';
 import { getTask,getTasks,createTask,deleteTask,updateTask } from '../controllers/task.controller.js';
 import {requiredAuth} from "../middlewares/tokenValidation.js";
+import {createTaskSchema} from '../schemas/task.schema';
+import {validateSchema} from '../middlewares/validator.middeware.js'
 
 const router = Router();
 
 router.get('/tasks',requiredAuth, getTasks);
 router.get('/task/:id',requiredAuth, getTask);
-router.post('/task',requiredAuth, createTask);
+router.post('/task',requiredAuth,validateSchema(createTask), createTask);
 router.put('/task/:id',requiredAuth, updateTask);
 router.delete('/task/:id',requiredAuth, deleteTask);
 
