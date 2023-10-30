2a907b9147daf00e2c93e0aef709c47aaaed0091
mandrakhe <luisfernnandomb@gmail.com>
2023-10-29 22:37:39 -0500

    actualizacion

--- Trabajos_En_VisualStudioCode/Practical_clase/LOGIN-MRN/client/src/api/auth.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-
-const API = 'http://localhost:5000/api';
-
-
-export const registerReques = user => axios.post(`${API}/register`,user);  
--- Trabajos_En_VisualStudioCode/Practical_clase/LOGIN-MRN/index.js
+++
@@ -1,5 +0,0 @@
-import app from './app.js';
-import{connectDB} from './db.js';
-
-connectDB(app);
-app.listen(5000,()=>console.log('listening on port'));
